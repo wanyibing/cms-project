d75b55d8bfb3c2715035095e71bf44c299868ad6
wanyibing <826278347@qq.com>
2019-12-10 19:06:44 +0800

    User

--- src/main/java/com/wanyibing/controller/UserController.java
+++ src/main/java/com/wanyibing/controller/UserController.java
@@ -2,13 +2,20 @@ package com.wanyibing.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.wanyibing.service.UserService;
 
 @Controller
+@RequestMapping("user")
 public class UserController {
  
 	@Autowired 
 	private UserService service; 
 	
+	@RequestMapping("login")
+	public String login() {
+		
+		return "user/login";
+	}
 }
